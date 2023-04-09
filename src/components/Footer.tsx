6a4446f06fa2a6067446d6ad1d464d215c3e1f88
David Hsieh <yen888yen888@gmail.com>
2023-04-08 23:44:19 -0700

    Buy me a coffee

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,6 +8,13 @@ function Footer() {
         <a href="https://github.com/davidyen-888" className="text-blue-500">
           Sung-Yan(David) Hsieh
         </a>
+        <a href="https://www.buymeacoffee.com/syhsieh" target="_blank">
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+            alt="Buy Me A Coffee"
+            className="mx-auto w-28 h-8"
+          />
+        </a>
       </p>
     </div>
   );
